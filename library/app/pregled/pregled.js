4c4342f03ebdf82a5ef5a143b0beb58f435248a5
Tomsi <kopasz210@gmail.com>
2025-05-12 20:01:34 +0200

    Dodato zadatak A1

--- library/app/pregled/pregled.js
+++ library/app/pregled/pregled.js
@@ -1,1 +1,35 @@
 "use strict";
+
+let knjige = JSON.parse(localStorage.getItem("knjige"));
+if (!knjige || knjige.length === 0) {
+    knjige = [
+        { title: "Knjiga 1" },
+        { title: "Knjiga 2" },
+        { title: "Knjiga 3" }
+    ];
+    localStorage.setItem("knjige", JSON.stringify(knjige));
+}
+let kontener = document.getElementById("pregledkontener");
+
+function prikaziKnjige() {
+    kontener.innerHTML = "";
+
+    knjige.forEach((knjiga, index) => {
+        let red = document.createElement("tr");
+        red.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${knjiga.title}</td>
+            <td><button onclick="obrisiKnjigu(${index})">Obriši</button></td>
+        `;
+        kontener.appendChild(red);
+    });
+}
+
+function obrisiKnjigu(index) {
+    knjige.splice(index, 1);
+    localStorage.setItem("knjige", JSON.stringify(knjige));
+    prikaziKnjige();
+}
+
+prikaziKnjige();
+
